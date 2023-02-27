b116562c7cc88fb2010a35fd0c76868aa1dfe529
“markantonio99” <markomakkia@gmail.com>
2023-02-27 15:30:01 +0100

    salv

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      mail: [],
+    };
+  },
+  methods: {
+    mailList() {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+          this.mail.push(res.data.response);
+        });
+    },
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      this.mailList();
     }
-  }
-}).mount('#app')
+  },
+}).mount('#app');
